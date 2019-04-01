ee6a75914790b88814e5d0884f901909ab4ddf1e
AppTcom Sw <apptcom@icloud.com>
2019-04-01 12:44:56 +0200

    Initial Commit

---
+++ JenkensTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkensTest
+//
+//  Created by Magdy Kamal on 4/1/19.
+//  Copyright © 2019 Magdy Kamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
